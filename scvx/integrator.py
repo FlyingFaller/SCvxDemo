3440df7daa228e5f09ec9e5ac70fa21c54b08e04
Benjamin Saunders <freezecubed@gmail.com>
2026-02-19 15:04:41 -0800

    Added piecewise state integrator. Useful in calculating discretization error later.

--- scvx/integrator.py
+++ scvx/integrator.py
@@ -2,10 +2,11 @@ import numpy as np
 from scipy.integrate import solve_ivp
 from model import Model
 from trajectory import Trajectory
+from labeled_views import LabeledArray
 
 class Integrator:
     def __init__(self, m: Model):
-        self.m = m
+        # self.m = m
         self.K = m.K
         self.ns = m.ns
         self.nx = m.nx
@@ -13,6 +14,9 @@ class Integrator:
 
         self.F, self.A, self.B, self.S = m.get_dynamics()
         self.dt = 1.0/(self.K-1)
+
+        self.xlabels = m.xlabels
+
         # y0 contains: 
         #   x: (nx)
         #   Phi/Ak: (nx, nx)
@@ -21,11 +25,8 @@ class Integrator:
         #   Bk+: (nx, nu)
         #   Sk: (nx, ns)
         #   wk: (nx)
-        # All this K-1 times over. y0 is a vector of length (K-1)(2 + 2*nx + 2*nu + ns)*nx
+        # All this K-1 times over. y0 is a vector of length N = (K-1)(2 + 2*nx + 2*nu + ns)*nx
         
-        # We have to fill in x_bar for each K-1 slot and init Phi/Psi to idents
-        # Need a scalable way to slice y 
-
         self.sizes = [
             self.nx,             # x is (nx,)
             self.nx**2,          # Phi is (nx, nx) -> flattened to (nx^2,)
@@ -39,15 +40,44 @@ class Integrator:
 
         self.N = np.sum(self.sizes)
 
-    def piecewise(self):
+    def integrate_piecewise(self, traj: Trajectory) -> LabeledArray:
         """Integrate the nonlinear dynamics piecewise across each subinterval."""
+        # Need to integrate F(s, x, u) for all i->i+1 K-1 times
+        # Best way to do this is to stack x[:-1] = y0 and then create a dydt where ui and xi are passed in vectorized to F
+        s = np.tile(traj.sigma, (self.K, 1)).T # (ns, K)
+        x = traj.x.T                           # (nx, K)
+        u = traj.u.T                           # (nu, K)
+ 
+        x0 = x[:, :-1] # (nx, K-1)
+        sol = solve_ivp(fun = self._dxdt_piecewise,
+                        t_span = (0, self.dt),
+                        y0=x0.ravel,
+                        args=(u, s[:, :-1]))
+        xf = sol.y[:, -1].reshape((self.nx, self.K-1))
+        return LabeledArray(xf.T, labels=self.xlabels)
+
+    def _dxdt_piecewise(self, 
+                        t: float, 
+                        xi_flat: np.ndarray, 
+                        u: np.ndarray, 
+                        s: np.ndarray):
+        xi = xi_flat.reshape((self.nx, self.K-1))
+
+        # FOH on controls
+        alpha = (self.dt - t) / self.dt
+        beta = t / self.dt
+        ui = alpha*u[:, :-1] + beta[:, 1:] # (nu, K-1)
+
+        # Calculate and return
+        Fi = self.F(s, xi, ui).reshape((self.nx, self.K-1))
+        return Fi.ravel()
+
+
+    def integrate_continuous(self, traj: Trajectory) -> LabeledArray:
+        """Integrate the nonlinear dynamics continously across the time horizon from the initial state."""
         pass
 
-    def continuous(self):
-        """Integrate the nonlinear dynamics continously across the time horizon."""
-        pass
-
-    def discretize(self, traj: Trajectory):
+    def discretize(self, traj: Trajectory) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
         """Integrate to compute the discretization of the dynamics."""
         # State-major order to avoid transposing in _dydt
         s = np.tile(traj.sigma, (self.K, 1)).T # (ns, K)
@@ -95,7 +125,7 @@ class Integrator:
               t: float, 
               yi_flat: np.ndarray, 
               u: np.ndarray, 
-              s: np.ndarray):
+              s: np.ndarray) -> np.ndarray:
         """Calculates the massive derivative of the discretization problem thing."""
         # FOH on controls
         alpha = (self.dt - t) / self.dt
@@ -148,104 +178,3 @@ class Integrator:
         np.einsum('ijk,jk->ik', Psii, residual, out=dw)
         
         return dydt.ravel()
-
-    # def discretize(self, traj: Trajectory):
-    #     """Integrate to compute the discretization of the dynamics."""
-    #     s = np.tile(traj.sigma, (self.K, 1)) # (K, ns)
-    #     x = traj.x     # (K, nx)
-    #     u = traj.u     # (K, nu)
-
-    #     # init y, note: lambda funcs expect column states
-    #     y0 = np.zeros((self.K-1, self.N))
-
-    #     # split outputs into shapes of (K-1, shape)
-    #     parts = np.split(y0, self.split_idxs, axis=-1)
-    #     x0 = parts[0]
-    #     Phi0 = parts[1]
-    #     Psi0 = parts[2]
-
-    #     x0[:] = x[:-1] # Set all x0 
-
-    #     # init Phi and Psi to identity 
-    #     I_flat = np.eye(self.nx).flatten()
-    #     Phi0[:] = I_flat
-    #     Psi0[:] = I_flat
-
-    #     # call integrator, Vf will be shape (K-1, N)
-    #     sol = solve_ivp(fun=self._dydt,
-    #                    t_span=(0, self.dt),
-    #                    y0=y0.flatten(),
-    #                    args=(u, s))
-        
-    #     # Extract final state
-    #     Vf = sol.y[:, -1].reshape((self.K-1, self.N))
-
-    #     parts = np.split(Vf, self.split_idxs, axis=-1)
-        
-    #     # x_prop = parts[0] # Propagated state x(t_{k+1}), useful for debug
-    #     Phif = parts[1].reshape((self.K-1, self.nx, self.nx))
-    #     # Psi_f = parts[2] # Not needed for discrete matrices
-    #     Bm_int = parts[3].reshape((self.K-1, self.nx, self.nu))
-    #     Bp_int = parts[4].reshape((self.K-1, self.nx, self.nu))
-    #     S_int = parts[5].reshape((self.K-1, self.nx, self.ns))
-    #     w_int = parts[6].reshape((self.K-1, self.nx))
-        
-    #     Ak = Phif 
-    #     Bkm = Ak @ Bm_int
-    #     Bkp = Ak @ Bp_int
-    #     Sk = Ak @ S_int
-    #     wk = Ak @ w_int
-    #     return Ak, Bkm, Bkp, Sk, wk
-
-    # def _dydt(self, t, yi, u, s):
-    #     # Due to my more general formulation we actually have:
-    #     # x' = F(old) + A(x - x_old) + B(u - u_old) + S(s-s_old)
-    #     # x' = Ax + Bu + Ss + F(old) -Ax_old - Bu_old - Ss_old
-
-    #     # FOH on controls
-    #     alpha = (self.dt - t) / self.dt
-    #     beta = t / self.dt
-    #     ui = alpha*u[:-1] + beta*u[1:] # (K-1, nu) output
-
-    #     si = s[:-1] # Need to ensure this matches xi in dimension (K-1, ns)
-
-    #     # Split outputs into shapes of (K-1, shape)
-    #     # (xi, Phii, Psii, Bmi, Bpi, Si, wi)
-    #     parts = np.split(yi.reshape((self.K-1, self.N)), self.split_idxs, axis=-1)
-    #     xi = parts[0]
-    #     Phii = parts[1].reshape((self.K-1, self.nx, self.nx))
-    #     Psii = parts[2].reshape((self.K-1, self.nx, self.nx))
-    #     # Bmi = parts[3].reshape((self.K-1, self.nx, self.nu))
-    #     # Bpi = parts[4].reshape((self.K-1, self.nx, self.nu))
-    #     # Si = parts[5].reshape((self.K-1, self.nx, self.ns))
-    #     # wi = parts[6]
-
-    #     # Get dynamics & jacobians, expect shapes of (shape, K-1) to vectorize
-    #     # outputs are of shapes (nx, 1, K-1), (nx, nx, K-1), (nx, nu, K-1), (nx, ns, K-1)
-    #     # Lambdified sympy functions
-    #     Fi = np.moveaxis(self.F(si.T, xi.T, ui.T), -1, 0).reshape((self.K-1, self.nx)) # make shape (K-1, nx)
-    #     Ai = np.moveaxis(self.A(si.T, xi.T, ui.T), -1, 0) # make shape (K-1, nx, nx)
-    #     Bi = np.moveaxis(self.B(si.T, xi.T, ui.T), -1, 0) # make shape (K-1, nx, nu)
-    #     Si = np.moveaxis(self.S(si.T, xi.T, ui.T), -1, 0) # make shape (K-1, nx, ns)
-
-    #     dxi = Fi
-    #     dPhi = Ai @ Phii
-    #     dPsi = -Psii @ Ai
-    #     dBm = Psii @ Bi * alpha
-    #     dBp = Psii @ Bi * beta
-    #     dS = Psii @ Si
-
-    #     residual = Fi - (Ai @ xi + Bi @ ui + Si @ si)
-    #     dw = Psii @ residual
-
-    #     dPhi_flat = dPhi.reshape((self.K-1, -1))
-    #     dPsi_flat = dPsi.reshape((self.K-1, -1))
-    #     dBm_flat  = dBm.reshape((self.K-1, -1))
-    #     dBp_flat  = dBp.reshape((self.K-1, -1))
-    #     dS_flat   = dS.reshape((self.K-1, -1))
-               
-    #     # Stack column-wise
-    #     dydt = np.hstack([dxi, dPhi_flat, dPsi_flat, dBm_flat, dBp_flat, dS_flat, dw])
-        
-    #     return dydt.flatten()
-
