8eb0d3d5d0174bde5a44749f739458f221c98bcf
FlyingFaller <bengsaunders@gmail.com>
2026-02-28 02:58:55 -0800

    Testing basic stuff.

--- scvx/test.py
+++ scvx/test.py
@@ -1,10 +1,16 @@
-from scvx import SCvxProblem
-from models.quadcopter_fixedft import QuadcopterFixedFT
+# from scvx import SCvxProblem
+# from models.quadcopter_fixedft import QuadcopterFixedFT
+import cvxpy as cvx
+import numpy as np
 
 def main():
-    model = QuadcopterFixedFT()
-    problem = SCvxProblem(model)
-    problem.solve(verbose=True)
+    # model = QuadcopterFixedFT()
+    # problem = SCvxProblem(model)
+    # problem.solve(verbose=True)
+    x = np.array([2, 3, 4])
+    y = np.array([2, 3, 4])
+    # print(cvx.vdot(x, y).value)
+    print(cvx.flatten(y[:, None]))
 
 
 if __name__ == "__main__":
